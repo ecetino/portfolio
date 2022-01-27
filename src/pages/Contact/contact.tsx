be67916684d22122ad16a7504c3840da59f18595
ecetino <1cetino.e@gmail.com>
2022-01-27 09:20:49 -0800

    contact page creation and styling

--- src/pages/Contact/contact.tsx
+++ src/pages/Contact/contact.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
+
 import './styles.scss';
 
-interface Props {
-};
-
-export const Contact = ({  }: Props) => {
+export const Contact = () => {
     return(
-        <div className="contact-container">
-            contact
+        <div className="contact">
+            <h2>Contact</h2>
+            <p>Thank you for taking the time to look over my portfolio. Please feel free to reach out to me by email, LinkedIn, or phone using my contact information listed below. Looking forward to connecting!</p>
+            <div className="mt1">
+                <p>1cetino.e@gmail.com</p>
+                <p>(425) 457-9672</p>
+            </div>
         </div>
     );
 };
