fb806448369e3de1e5a10f895f0bd0fb42a5fff0
ecetino <1cetino.e@gmail.com>
2022-01-27 09:20:23 -0800

    App and about file updates

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,29 +1,29 @@
-import { Home } from '../pages';
-import { About } from '../pages';
-import { Projects } from '../pages';
-import { Contact } from '../pages';
-
-import { Header } from '../components/Header/header';
-
 import {
   Routes,
   Route
 } from 'react-router-dom';
 
+import { Header } from '../components/Header/header';
+import { Home } from '../pages';
+import { About } from '../pages';
+import { Projects } from '../pages';
+import { Contact } from '../pages';
+import { Resume } from '../pages';
 
 const App = () => {
   return (
-    <div>
+    <>
       <Header />
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="projects" element={<Projects />} />
+          <Route path="resume" element={<Resume />} />
           <Route path="contact" element={<Contact />} />
         </Routes>
       </main>
-    </div>
+    </>
   );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Home } from "./Home/home";
 import { About } from "./About/about";
 import { Projects } from "./Projects/projects";
+import { Resume } from "./Resume/resume";
 import { Contact } from "./Contact/contact";
 
-export { Home, About,Projects, Contact };
+export { Home, About, Projects, Resume, Contact };
