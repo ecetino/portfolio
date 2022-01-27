df86f74622da87b2ee21441d2855d21155ab4e88
ecetino <1cetino.e@gmail.com>
2022-01-27 09:19:23 -0800

    resume page iniitializaiton and styling

---
+++ src/pages/Resume/resume.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.scss';
+
+interface Props {
+};
+
+export const Resume = ({  }: Props) => {
+    return(
+        <div className="resume-container">
+            Resume
+        </div>
+    );
+};
