af4891e47b4d576a647d161154ac31e4adbc5801
ecetino <1cetino.e@gmail.com>
2022-02-01 22:09:00 -0800

    resume component creation

--- src/pages/Resume/resume.tsx
+++ src/pages/Resume/resume.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
+
 import './styles.scss';
 
-interface Props {
-};
-
-export const Resume = ({  }: Props) => {
+export const Resume = () => {
     return(
-        <div className="resume-container">
-            Resume
+        <div className="resume">
+            <h2>Resume</h2>
+            <div className='flex-col'>
+                <a className="mb20" href='/Edgar_Cetino_Resume.pdf' download>Click to Download PDF</a>
+                <img src={require('../../assets/images/Edgar_Cetino_Resume.png').default} alt='resume' />
+            </div>
         </div>
     );
 };
